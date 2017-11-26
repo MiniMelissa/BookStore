3d6ab5245379ff135c149ef7312eed7b942e8067
melissa <xumeng919@gmail.com>
2017-11-25 19:09:44 -0500

    create tables in bookstore database, add web security configuration with spring security, replace static page by dynamic page

---
+++ src/main/java/com/xumeng/bookstore/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.xumeng.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.xumeng.bookstore.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+	User findByUsername(String username);
+}
