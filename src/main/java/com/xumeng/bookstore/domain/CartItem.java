9b21ee225d92e47a69d7fc7f36a23b57c2dbc7e8
melissa <xumeng919@gmail.com>
2017-12-08 13:25:05 -0500

    add domain modules and Order, CartItem, BookToCartItem, BillingAddress, Payment,ShippingAddress, ShoppingCart entities

---
+++ src/main/java/com/xumeng/bookstore/domain/CartItem.java
@@ -0,0 +1,101 @@
+package com.xumeng.bookstore.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Long id;
+	private int qty;
+	private BigDecimal subtotal;
+	
+	@OneToOne
+	private Book book;
+	
+	//bookToCartItem is middle table between book and cartItem, 
+	//because hibernate cannot handle many to many 
+	@OneToMany(mappedBy = "cartItem")
+	@JsonIgnore
+	private List<BookToCartItem> bookToCartItemList;
+	
+	@ManyToOne
+	@JoinColumn(name="shopping_cart_id")
+	private ShoppingCart shoppingCart;
+	
+	@ManyToOne
+	@JoinColumn(name="order_id")
+	private Order order;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public BigDecimal getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(BigDecimal subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public List<BookToCartItem> getBookToCartItemList() {
+		return bookToCartItemList;
+	}
+
+	public void setBookToCartItemList(List<BookToCartItem> bookToCartItemList) {
+		this.bookToCartItemList = bookToCartItemList;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+
+	public Order getOder() {
+		return order;
+	}
+
+	public void setOder(Order oder) {
+		this.order = oder;
+	}
+	
+	
+	
+}
