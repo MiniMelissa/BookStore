26a40ca7c722e49d3e806fb2d700cf3b296b2347
melissa <xumeng919@gmail.com>
2017-12-11 23:48:07 -0500

    add functionality of checkbox same as shipping address, fix bugs

--- src/main/java/com/xumeng/bookstore/controller/CheckoutController.java
+++ src/main/java/com/xumeng/bookstore/controller/CheckoutController.java
@@ -30,7 +30,7 @@ import com.xumeng.bookstore.utility.USConstants;
 
 @Controller
 public class CheckoutController {
-	
+
 	private ShippingAddress shippingAddress = new ShippingAddress();
 	private BillingAddress billingAddress = new BillingAddress();
 	private Payment payment = new Payment();
@@ -45,10 +45,10 @@ public class CheckoutController {
 	private ShippingAddressService shippingAddressService;
 	
 	@Autowired
-	private PaymentService paymentService;
+	private BillingAddressService billingAddressService;
 	
 	@Autowired
-	private BillingAddressService billingAddressService;
+	private PaymentService paymentService;
 	
 	@Autowired
 	private UserShippingService userShippingService;
@@ -56,53 +56,59 @@ public class CheckoutController {
 	@Autowired
 	private UserPaymentService userPaymentService;
 	
-	
 	@RequestMapping("/checkout")
-	public String checkout(@RequestParam("id")Long cartId, 
-			@RequestParam(value="missingRequiredField", required=false)boolean missingRequiredField,
-			Model model, Principal principal) {
-	
+	public String checkout(
+			@RequestParam("id") Long cartId,
+			@RequestParam(value="missingRequiredField", required=false) boolean missingRequiredField,
+			Model model, Principal principal
+			){
 		User user = userService.findByUsername(principal.getName());
+		
 		if(cartId != user.getShoppingCart().getId()) {
-			return "badRequestpage";
+			return "badRequestPage";
 		}
+		
 		List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
 		
 		if(cartItemList.size() == 0) {
 			model.addAttribute("emptyCart", true);
-			return "forward:/shoppingCart/cart";
+			return "forward:/shoppintCart/cart";
 		}
-		for(CartItem cartItem : cartItemList) {
+		
+		for (CartItem cartItem : cartItemList) {
 			if(cartItem.getBook().getInStockNumber() < cartItem.getQty()) {
 				model.addAttribute("notEnoughStock", true);
 				return "forward:/shoppingCart/cart";
 			}
 		}
+		
 		List<UserShipping> userShippingList = user.getUserShippingList();
 		List<UserPayment> userPaymentList = user.getUserPaymentList();
 		
 		model.addAttribute("userShippingList", userShippingList);
 		model.addAttribute("userPaymentList", userPaymentList);
 		
-		if(userPaymentList.size() == 0) {
+		if (userPaymentList.size() == 0) {
 			model.addAttribute("emptyPaymentList", true);
-		}else {
+		} else {
 			model.addAttribute("emptyPaymentList", false);
 		}
-		if(userShippingList.size() == 0) {
+		
+		if (userShippingList.size() == 0) {
 			model.addAttribute("emptyShippingList", true);
-		}else {
+		} else {
 			model.addAttribute("emptyShippingList", false);
 		}
 		
 		ShoppingCart shoppingCart = user.getShoppingCart();
 		
-		for(UserShipping userShipping: userShippingList) {
+		for(UserShipping userShipping : userShippingList) {
 			if(userShipping.isUserShippingDefault()) {
 				shippingAddressService.setByUserShipping(userShipping, shippingAddress);
 			}
 		}
-		for(UserPayment userPayment : userPaymentList) {
+		
+		for (UserPayment userPayment : userPaymentList) {
 			if(userPayment.isDefaultPayment()) {
 				paymentService.setByUserPayment(userPayment, payment);
 				billingAddressService.setByUserBilling(userPayment.getUserBilling(), billingAddress);
@@ -113,7 +119,7 @@ public class CheckoutController {
 		model.addAttribute("payment", payment);
 		model.addAttribute("billingAddress", billingAddress);
 		model.addAttribute("cartItemList", cartItemList);
-		model.addAttribute("shoppingCart", shoppingCart);
+		model.addAttribute("shoppingCart", user.getShoppingCart());
 		
 		List<String> stateList = USConstants.listOfUSStatesCode;
 		Collections.sort(stateList);
@@ -122,24 +128,28 @@ public class CheckoutController {
 		model.addAttribute("classActiveShipping", true);
 		
 		if(missingRequiredField) {
-			model.addAttribute("missRequiredField", true);
+			model.addAttribute("missingRequiredField", true);
 		}
+		
 		return "checkout";
+		
 	}
 	
 	@RequestMapping("/setShippingAddress")
-	public String setShippingAddress(@RequestParam("userShippingId") Long userShippingId,
-			Principal principal, Model model) {
+	public String setShippingAddress(
+			@RequestParam("userShippingId") Long userShippingId,
+			Principal principal, Model model
+			) {
 		User user = userService.findByUsername(principal.getName());
 		UserShipping userShipping = userShippingService.findById(userShippingId);
 		
 		if(userShipping.getUser().getId() != user.getId()) {
 			return "badRequestPage";
-		}else {
+		} else {
 			shippingAddressService.setByUserShipping(userShipping, shippingAddress);
+			
 			List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
 			
-			BillingAddress billingAddress = new BillingAddress();
 			model.addAttribute("shippingAddress", shippingAddress);
 			model.addAttribute("payment", payment);
 			model.addAttribute("billingAddress", billingAddress);
@@ -152,38 +162,46 @@ public class CheckoutController {
 			
 			List<UserShipping> userShippingList = user.getUserShippingList();
 			List<UserPayment> userPaymentList = user.getUserPaymentList();
+			
 			model.addAttribute("userShippingList", userShippingList);
 			model.addAttribute("userPaymentList", userPaymentList);
 			
 			model.addAttribute("shippingAddress", shippingAddress);
+			
 			model.addAttribute("classActiveShipping", true);
 			
-			if(userPaymentList.size() == 0) {
+			if (userPaymentList.size() == 0) {
 				model.addAttribute("emptyPaymentList", true);
-			}else {
+			} else {
 				model.addAttribute("emptyPaymentList", false);
 			}
 			
+			
 			model.addAttribute("emptyShippingList", false);
+			
 			
 			return "checkout";
 		}
 	}
 	
 	@RequestMapping("/setPaymentMethod")
-	public String setPaymentMethod(@RequestParam("userPaymentId") Long userPaymentId, 
-			Principal principal, Model model) {
+	public String setPaymentMethod(
+			@RequestParam("userPaymentId") Long userPaymentId,
+			Principal principal, Model model
+			) {
 		User user = userService.findByUsername(principal.getName());
 		UserPayment userPayment = userPaymentService.findById(userPaymentId);
 		UserBilling userBilling = userPayment.getUserBilling();
 		
-		if(userPayment.getUser().getId() != user.getId()) {
+		if(userPayment.getUser().getId() != user.getId()){
 			return "badRequestPage";
-		}else {
+		} else {
 			paymentService.setByUserPayment(userPayment, payment);
+			
 			List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
-
+			
 			billingAddressService.setByUserBilling(userBilling, billingAddress);
+			
 			model.addAttribute("shippingAddress", shippingAddress);
 			model.addAttribute("payment", payment);
 			model.addAttribute("billingAddress", billingAddress);
@@ -196,26 +214,26 @@ public class CheckoutController {
 			
 			List<UserShipping> userShippingList = user.getUserShippingList();
 			List<UserPayment> userPaymentList = user.getUserPaymentList();
+			
 			model.addAttribute("userShippingList", userShippingList);
 			model.addAttribute("userPaymentList", userPaymentList);
 			
 			model.addAttribute("shippingAddress", shippingAddress);
+			
 			model.addAttribute("classActivePayment", true);
 			
 			
 			model.addAttribute("emptyPaymentList", false);
 			
-			if(userShippingList.size() == 0) {
+			
+			if (userShippingList.size() == 0) {
 				model.addAttribute("emptyShippingList", true);
-			}else {
+			} else {
 				model.addAttribute("emptyShippingList", false);
 			}
 			
 			return "checkout";
 		}
-		
 	}
-
-	
 	
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,10 +2,19 @@
  * change 
  */
 
+function checkBillingAddress(){
+	if($("#theSameAsShippingAddress").is(":checked")){
+		$(".billingAddress").prop("disabled", true);
+	}else{
+		$(".billingAddress").prop("disabled", false);
+	}
+}
+
 $(document).ready(function(){
 	$(".cartItemQty").on('change', function(){
 		var id = this.id;
 		console.log("test");//to test if this js function is called
 		$('#update-item-'+id).css('display','inline-block');
 	});
+	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
 });
